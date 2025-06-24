11ea9929bbc8410f585360987f81f7b84f8f7d6e
shuja-git <shuja.devops@gmail.com>
2025-06-24 18:16:50 +0530

    shell script to terraform commands of remote backend and apply

--- run.sh
+++ run.sh
@@ -9,6 +9,6 @@ if [ -z $action ]; then
   echo -e "\e[31mAction missing(apply|destroy)\e[0m"
   exit 1
 fi
-
+rm -rf .terraform/terraform.tfstate
 terraform init -backend-config=$env-env/state.tfvars
-terraform $action -var-file=dev-$env-env/main.tfvars
+terraform $action -var-file=$env-env/main.tfvars
