dced540d4290b472386c9c794c230085770bb550
shuja-git <shuja.devops@gmail.com>
2025-06-25 14:46:56 +0530

    pased instance private ip to route53 record

--- module/app/main.tf
+++ module/app/main.tf
@@ -30,5 +30,6 @@ resource "aws_route53_record" "record" {
   name    = "${var.component}-${var.env}"
   type    = "A"
   zone_id = var.zone_id
+  records = [aws_instance.instance.private_ip]
   ttl = 30
 }
