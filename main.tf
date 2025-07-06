b215d24cfbab07f5be6ee65a4168a9feed43350c
shuja-git <shuja.devops@gmail.com>
2025-07-06 06:13:46 +0530

    updated vault token

--- main.tf
+++ main.tf
@@ -1,24 +1,24 @@
-module "frontend" {
-  depends_on = [module.backend]
-  source        = "./module/app"
-  instance_type = var.instance_type
-  component     = "frontend"
-  ssh_user      = var.ssh_user
-  ssh_pass      = var.ssh_pass
-  env           = var.env
-  zone_id       = var.zone_id
-}
-
-module "backend" {
-  depends_on = [module.mysql]
-  source        = "./module/app"
-  instance_type = var.instance_type
-  component     = "backend"
-  ssh_user      = var.ssh_user
-  ssh_pass      = var.ssh_pass
-  env           = var.env
-  zone_id       = var.zone_id
-}
+# module "frontend" {
+#   depends_on = [module.backend]
+#   source        = "./module/app"
+#   instance_type = var.instance_type
+#   component     = "frontend"
+#   ssh_user      = var.ssh_user
+#   ssh_pass      = var.ssh_pass
+#   env           = var.env
+#   zone_id       = var.zone_id
+# }
+#
+# module "backend" {
+#   depends_on = [module.mysql]
+#   source        = "./module/app"
+#   instance_type = var.instance_type
+#   component     = "backend"
+#   ssh_user      = var.ssh_user
+#   ssh_pass      = var.ssh_pass
+#   env           = var.env
+#   zone_id       = var.zone_id
+# }
 
 module "mysql" {
   source        = "./module/app"
