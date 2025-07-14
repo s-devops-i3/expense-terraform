1fb25e89188e962e5c00467bbf59487e6dab29cd
shuja-git <shuja.devops@gmail.com>
2025-07-14 20:36:11 +0530

    commented load balancer

--- module/app/main.tf
+++ module/app/main.tf
@@ -60,17 +60,17 @@ resource "null_resource" "ansible" {
   }
 }
 # -----Creating Load Balancer
-resource "aws_lb" "main" {
-  name               = "${var.env}-${var.component}-alb"
-  internal           = var.lb_type == "public" ? false : true
-  load_balancer_type = "application"
-  security_groups    = [aws_security_group.main.id]
-  subnets            = var.lb_subnet
-
-  tags = {
-    Environment = "${var.env}-${var.component}-alb"
-  }
-}
+# resource "aws_lb" "main" {
+#   name               = "${var.env}-${var.component}-alb"
+#   internal           = var.lb_type == "public" ? false : true
+#   load_balancer_type = "application"
+#   security_groups    = [aws_security_group.main.id]
+#   subnets            = var.lb_subnet
+#
+#   tags = {
+#     Environment = "${var.env}-${var.component}-alb"
+#   }
+# }
 #-----------------------------
 
 
