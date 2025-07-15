477857bdb8b231acf8d7b27cdb8c50fbbcd59f33
shuja-git <shuja.devops@gmail.com>
2025-07-15 16:55:11 +0530

    adding health check

--- module/app/main.tf
+++ module/app/main.tf
@@ -84,6 +84,16 @@ resource "aws_lb_target_group" "main" {
   port     = 80
   protocol = "HTTP"
   vpc_id   = var.vpc_id
+  deregistration_delay = 15
+
+  health_check{
+    healthy_threshold   = 2
+    interval            = 5
+    path                = "/health"
+    timeout             = 2
+    port                = var.app_port
+    unhealthy_threshold = 2
+  }
 }
 #---Creating Listener
 resource "aws_lb_listener" "front" {
