1f41621830c6e36553fc9cfb16cc32d3c3c73670
shuja-git <shuja.devops@gmail.com>
2025-07-27 15:02:38 +0530

    adding certificate

--- module/app/main.tf
+++ module/app/main.tf
@@ -34,32 +34,35 @@ resource "aws_security_group" "main" {
 
 }
 }
-#----Security group for Load Balancer
-resource "aws_security_group" "lb-sg" {
+#------------------------------------
+resource "aws_security_group" "load-balancer" {
   count       = var.lb_needed ? 1 : 0
-  name        = "${var.component}-${var.env}-lbsg"
-  description = "${var.component}-${var.env}-lbsg"
+  name        = "${var.component}-${var.env}-lb-sg"
+  description = "${var.component}-${var.env}-lb-sg"
   vpc_id      = var.vpc_id
 
-  tags = {
-    Name = "${var.component}-${var.env}-lbsg"
+  dynamic "ingress" {
+    for_each = var.lb_ports
+    content {
+      from_port   = ingress.value
+      to_port     = ingress.value
+      protocol    = "TCP"
+      cidr_blocks = var.lb_app_port_sg_cidr
+    }
   }
-  ingress {
-    from_port        = var.app_port
-    to_port          = var.app_port
-    protocol         = "TCP"
-    cidr_blocks      = var.lb_app_port_sg_cidr
 
-  }
   egress {
-    from_port        = 0
-    to_port          = 0
-    protocol         = "-1"
-    cidr_blocks      = ["0.0.0.0/0"]
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
+  tags = {
+    Name = "${var.component}-${var.env}-sg"
   }
 }
-
+#------------------------------------
 resource "aws_instance" "instance" {
   ami                    = data.aws_ami.ami.image_id
   instance_type          = var.instance_type
@@ -127,7 +130,7 @@ resource "aws_lb" "main" {
   name               = "${var.env}-${var.component}-alb"
   internal           = var.lb_type == "Public" ? false : true
   load_balancer_type = "application"
-  security_groups    = [aws_security_group.lb-sg[0].id]
+  security_groups    = [aws_security_group.load-balancer[0].id]
   subnets            = var.lb_subnet
 
   tags = {
@@ -159,8 +162,41 @@ resource "aws_lb_target_group_attachment" "main" {
   port             = var.app_port
 }
 
-resource "aws_lb_listener" "main" {
-  count             = var.lb_needed ? 1 : 0
+resource "aws_lb_listener" "frontend-http" {
+  count             = var.lb_needed && var.lb_type == "public" ? 1 : 0
+  load_balancer_arn = aws_lb.main[0].arn
+  port              = var.app_port
+  protocol          = "HTTP"
+
+  default_action {
+    type = "redirect"
+
+    redirect {
+      port        = "443"
+      protocol    = "HTTPS"
+      status_code = "HTTP_301"
+    }
+  }
+
+}
+
+resource "aws_lb_listener" "frontend-https" {
+  count             = var.lb_needed && var.lb_type == "public" ? 1 : 0
+  load_balancer_arn = aws_lb.main[0].arn
+  port              = "443"
+  protocol          = "HTTPS"
+  ssl_policy        = "ELBSecurityPolicy-TLS13-1-2-2021-06"
+  certificate_arn   = var.certificate_arn
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.main[0].arn
+  }
+
+}
+
+resource "aws_lb_listener" "backend" {
+  count             = var.lb_needed && var.lb_type != "public" ? 1 : 0
   load_balancer_arn = aws_lb.main[0].arn
   port              = var.app_port
   protocol          = "HTTP"
@@ -169,4 +205,5 @@ resource "aws_lb_listener" "main" {
     type             = "forward"
     target_group_arn = aws_lb_target_group.main[0].arn
   }
+
 }
