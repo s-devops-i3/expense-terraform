24d90d59327f653df8378fd4b6b01bfb9a2d7525
shuja-git <shuja.devops@gmail.com>
2025-08-01 08:35:57 +0530

    list braces fix

--- module/app-asg/main.tf
+++ module/app-asg/main.tf
@@ -57,7 +57,7 @@ resource "aws_autoscaling_group" "main" {
   min_size                = var.min_size
   desired_capacity        = var.min_size
   vpc_zone_identifier     = var.subnets
-  target_group_arns       = aws_lb_target_group.main.arn
+  target_group_arns       = [aws_lb_target_group.main.arn]
 
   launch_template {
     id                  = aws_launch_template.main.id
